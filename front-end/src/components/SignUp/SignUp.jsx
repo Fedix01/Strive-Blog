71dfd058e3caef0a3622399a9190b65ca1720699
Federico Manna <federicomanna01@yahoo.it>
2024-05-04 21:14:00 +0200

    layout improvements

--- front-end/src/components/SignUp/SignUp.jsx
+++ front-end/src/components/SignUp/SignUp.jsx
@@ -3,7 +3,8 @@ import { Button, Container, Form, Alert, Row, Col } from 'react-bootstrap';
 import MyNavbar from '../MyNavbar/MyNavbar';
 import MyFooter from '../MyFooter/MyFooter';
 import { useNavigate } from 'react-router-dom';
-import backgroundSito from '../../assets/backgroundSito.jpg';
+import backgroundSite from '../../assets/backgroundSite.jpg';
+import { FcGoogle } from "react-icons/fc";
 
 export default function SignUp() {
 
@@ -50,51 +51,53 @@ export default function SignUp() {
     return (
         <>
             <MyNavbar />
-            <Container>
-                <Row>
-                    <Col md={8} style={{ height: "100vh" }} className='d-flex flex-column justify-content-center align-items-center'>
-                        <h2>Log In al Tuo Account</h2>
-                        <Form onSubmit={handleSubmit}>
-                            <Form.Group>
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    placeholder="Enter email"
-                                    value={formData.email}
-                                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-                                    required
-                                />
-                            </Form.Group>
+            <Row>
+                <Col md={8} style={{ height: "100vh" }} className='d-flex flex-column justify-content-center align-items-center'>
+                    <h2 style={{ fontSize: "55px" }}>Log In al Tuo Account</h2>
+                    <p>Login usando Google</p>
+                    <Button variant='transparent' className='p-2' style={{ border: "1px solid gray", borderRadius: "20px" }}>
+                        <FcGoogle />
+                        <span className='ms-2'>Login con Google</span>
+                    </Button>
+                    <hr />
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                value={formData.email}
+                                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                                required
+                            />
+                        </Form.Group>
 
-                            <Form.Group>
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    placeholder="Enter password"
-                                    value={formData.password}
-                                    onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-                                    required
-                                />
-                            </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Enter password"
+                                value={formData.password}
+                                onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                                required
+                            />
+                        </Form.Group>
 
-                            <Button type='submit' style={{ width: "100%" }}>Loggati</Button>
-                        </Form>
+                        <Button type='submit' style={{ width: "100%" }}>Loggati</Button>
+                    </Form>
 
-                        {alert &&
-                            <Alert className='my-2' variant='danger'>
-                                {alert}
-                            </Alert>
-                        }
-                    </Col>
-                    <Col md={4} style={{ width: "100%", height: "100%", backgroundImage: `url(${backgroundSito})` }}>
-                        <div className='d-flex flex-column justify-content-center'>
-                            <h6>Non hai un account?</h6>
-                            <Button variant='transparent' style={{ color: "#0067BF", fontWeight: "bold" }} className='mb-2' onClick={() => navigate("/signIn")}>Registrati</Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-            <MyFooter />
+                    {alert &&
+                        <Alert className='my-2' variant='danger'>
+                            {alert}
+                        </Alert>
+                    }
+                </Col>
+                <Col md={4} className='d-flex flex-column justify-content-center align-items-center' style={{ backgroundImage: `url(${backgroundSite})`, height: "100vh", color: "white" }}>
+                    <h2><b>Non hai un account?</b></h2>
+                    <p className='my-3'>Registrati per accedere alla creazione dei blog e poter scrivere commenti!</p>
+                    <Button variant='light' className='mt-2' style={{ width: "60%", borderRadius: "20px" }} onClick={() => navigate("/signIn")}>Registrati</Button>
+                </Col>
+            </Row>
         </>
     )
 }
